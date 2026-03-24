3b2333044ff9baf545b631a70af6855034b7fc76
dongbin <qc.dancer@qq.com>
2026-03-24 11:01:41 +0800

    慧眼卫星 域名

--- app/src/main/java/com/hht/hsatellitemobile/utils/RequestUtils.java
+++ app/src/main/java/com/hht/hsatellitemobile/utils/RequestUtils.java
@@ -1,7 +1,8 @@
 package com.hht.hsatellitemobile.utils;
 
 public class RequestUtils {
-    public static String REQUEST_URL_relea_new = "http://web.ehaohai.com:2019/api/";            //主页正式新 http://web.ehaohai.com:2019
+    public static String REQUEST_URL_relea_new = "http://wx2.ehaohai.com/api/";            //主页正式新 http://web.ehaohai.com:2019
+//    public static String REQUEST_URL_relea_new = "http://web.ehaohai.com:2019/api/";            //主页正式新 http://web.ehaohai.com:2019
 //    public static String REQUEST_URL_relea_new = "http://192.168.1.160:2019/api/";//2025
 
     public static String REQUEST_URL_VERSION = "http://web.ehaohai.com:2020/app/update.json";            //版本升级
